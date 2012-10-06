6e460adf3c7d4c786a72b31bcb62ad0d2c5e8c47
kylebv <kylebv@gmail.com>
2012-10-06 03:24:09 +0000

    updated LoadImage

--- LOCC/GraphicsManager.h
+++ LOCC/GraphicsManager.h
@@ -6,7 +6,7 @@ class CGraphicsManager
 public:
 
 	int GetID( std::string szGraphicsID );
-	void LoadImage( std::string szFilename );
+	void LoadImage( std::string szFilename, std::string szReferenceName );
 
 	static CGraphicsManager* GetInstance( void );
 	static void DeleteInstance(void);
