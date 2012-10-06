3e0af160c7d20ba0afc90ce1b53967c52f03e9a6
stumptheubergeek <stumptheubergeek@gmail.com>
2012-10-06 22:00:38 +0000

    Just putting up some updates to the Animation stuff. Getters and setters everywhere.

--- LOCC/Animation.h
+++ LOCC/Animation.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "Frame.h"
 class CFrame;
 
 // Had to put in some values change to what is needed ~Ryan
@@ -17,9 +17,17 @@ public:
 	bool GetLooping() {return m_bLooping;}
 	void SetPath(const char*m_ccSetByThis) {m_ccImagePath = m_ccSetByThis;}
 	const char* GetPath() {return m_ccImagePath;}
+	void SetAnimType(ANIM_TYPE temptype) {m_eAnimType = temptype;}
+	ANIM_TYPE GetAnimType() {return m_eAnimType;}
+	void SetUnitType(UNIT_TYPE tempunittype) {m_eUnitType = tempunittype;}
+	UNIT_TYPE GetUnitType() {return m_eUnitType;}
+	void SetFrame(CFrame* tempframe) {m_vAnimVec[tempframe->GetFrame()] = tempframe;}
+	std::vector<CFrame*> GetFrameVec() {return m_vAnimVec;}
 private:
 	std::vector< CFrame* > m_vAnimVec;
 	bool m_bLooping;
 	const char* m_ccImagePath;
+	UNIT_TYPE m_eUnitType;
+	ANIM_TYPE m_eAnimType;
 };
 
