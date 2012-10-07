d917cfff07a9803030df1c533626e8dcb2784ad6
dgbur924@gmail.com <dgbur924@gmail.com@c8e355b9-bc22-55f8-5626-cff841d7379f>
2012-10-07 16:51:04 +0000

    Finished the load function, Tile.h, and test XML. added Tile to Render, and the Test map.xml. Frozen status done for testing -DG

--- trunk/LOCC/TileManager.cpp
+++ trunk/LOCC/TileManager.cpp
@@ -43,6 +43,8 @@ bool CTileManager::LoadSave( std::string sFilename )
 	pRoot->Attribute("Columns", &tempCols);
 	texturefile= pRoot->Attribute("FileName");
 
+
+
 	TiXmlElement* pTiles = pRoot->FirstChildElement("Tiles");
 	TiXmlElement* pTile = pRoot->FirstChildElement("Tile");
 
@@ -50,18 +52,38 @@ bool CTileManager::LoadSave( std::string sFilename )
 	{
 		m_pTileMap[x]= new CTile[tempCols];
 	}
+	int tempdata1=0,tempdata2=0;
 
 	for (int x=0; x<tempRows; ++x)
 	{
 		for(int y=0;y<tempCols;++y)
 		{
-			//pTile->Attribute("PosX",&m_pTileMap[x][y].m_sPos.nPosX);
-			//pTile->Attribute("PosY",&m_pTileMap[x][y].m_sPos.nPosY);
+			pTile->Attribute("PosX",&tempdata1);
+			pTile->Attribute("PosY",&tempdata2);
+			m_pTileMap[x][y].SetPosition(tempdata1,tempdata2);
+			
+			pTile->Attribute("PixWidth",&tempdata1);
+			m_pTileMap[x][y].SetPixWidth(tempdata1);
+
+			pTile->Attribute("PixHeight",&tempdata1);
+			m_pTileMap[x][y].SetPixHeight(tempdata1);
+			
+			pTile->Attribute("TileWidth",&tempdata1);
+			m_pTileMap[x][y].SetTileWidth(tempdata1);
+
+			pTile->Attribute("TileHeight",&tempdata1);
+			m_pTileMap[x][y].SetTileHeight(tempdata1);
+
+			pTile->Attribute("Status",&tempdata1);
+			m_pTileMap[x][y].SetStatus(tempdata1);
+
+			pTile->Attribute("PlayerID",&tempdata1);
+			m_pTileMap[x][y].SetPlayerID(tempdata1);
+
+			pTile->Attribute("TType",&tempdata1);
+			m_pTileMap[x][y].SetTileType(tempdata1);
 		}
 	}
-
-
-	
 	return true;
 }
 
