b92d5619454f24bed1a870bef5d087abbdde09c0
kylebv@gmail.com <kylebv@gmail.com@c8e355b9-bc22-55f8-5626-cff841d7379f>
2012-10-16 04:43:51 +0000

    fixed lua maybe?

--- trunk/LOCC/Assets/Ability/test.lua
+++ trunk/LOCC/Assets/Ability/test.lua
@@ -1,12 +1,15 @@
+
 function OnUse()
- 
+	
 
- --print ("Test Table Input");
+	--print ("Test Table Input");
+	
+	--tUnitData[1].health = 5;
+	--tUnitData[2].health = 7;
+	--tUnitData[2].health = 9;
 
- tUnitData[3].health = tUnitData[3].health - 10;
- for lolwut = 2, table.getn(tUnitData) do
-  --print ("Unit " .. ", x: " .. tUnitData[i].posX .. ", y: " .. tUnitData[i].posY .. ", hp: " .. tUnitData[i].health .. ", speed: " .. tUnitData[i].speed);
-	tUnitData[lolwut].health = tUnitData[lolwut].health - 10;
- end	
- 
+	for i = 1, table.getn(tUnitData)	do
+		tUnitData[i].health = tUnitData[i].health - 10;
+	end
+	
 end
