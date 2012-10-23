a08f18d286c2439732cf1e0e374296903c883ed6
rcartier13 <rcartier13@gmail.com>
2012-10-23 19:53:55 +0000

    asdasdfasdf

--- LOCC/Assets/Ability/Charge.lua
+++ LOCC/Assets/Ability/Charge.lua
@@ -1,9 +1,12 @@
 function OnUse( )
-	
-damage = 6;
-	for i = 1, table.getn(tUnitData) do
-		tUnitData[i].health = tUnitData[i].health - damage;
-		damage = damage + 2;
-	end
+	AddText("Test", 150, 150, 0, 0, 1.2, 5, 255, 255, 255);
+	--DoDamage(tUnitData[1].uniqueID, 10);
+--damage = 6;
+--	for i = 1, table.getn(tUnitData) do
+	--	DoDamage(tUnitData[i].uniqueID, damage);
+	--	pixelX, pixelY = TranslateToPixel(tUnitData[i].posX, tUnitData[i].posY);
+	--	AddText("" .. damage, pixelX, pixelY, 0, -40, 3, 0.4, 255, 0, 0);
+	--	damage = damage + 2;
+	--end
 
 end
