6adc08c6892783fdf833eb49931fbfe4df573311
rcartier13@gmail.com <rcartier13@gmail.com@c8e355b9-bc22-55f8-5626-cff841d7379f>
2012-10-24 15:36:15 +0000

    huehuehue

--- trunk/LOCC/Assets/Ability/fireball.lua
+++ trunk/LOCC/Assets/Ability/fireball.lua
@@ -2,8 +2,8 @@ function OnUse ()
 
 
 	for i = 1, table.getn(tUnitData) do
-		DoDamage(tUnitData[i].uniqueID, 15);
+		damage = DoDamage(tUnitData[i].uniqueID, 15);
 		pixelX, pixelY = TranslateToPixel(tUnitData[i].posX, tUnitData[i].posY);
-		AddText("15", pixelX, pixelY, 0, -40, 3, 0.4, 255, 0, 0);
+		AddText("" .. damage, pixelX, pixelY, 0, -40, 3, 0.4, 255, 0, 0);
 	end
 end
