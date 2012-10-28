160f8fe81ced36c56a02e6308c15218b9ac1611b
kylebv <kylebv@gmail.com>
2012-10-28 23:51:14 +0000

    Cavalry charge and fixed AI attacking

--- LOCC/Assets/Scripts/ai_cavalry.lua
+++ LOCC/Assets/Scripts/ai_cavalry.lua
@@ -39,12 +39,103 @@ function MoveToNearestEnemy()
 end
 
 function Attack()
-	AttackNearest()
+
+	localX, localY = GetUnitPosition(unitID);
+	pixelX, pixelY = TranslateToPixel(localX, localY);
+	AddText("Cavalry Attack.11111..", pixelX, pixelY, 40, 0, 5, 0.4, 20, 255, 20);	
+
+	nearestID = FindNearest(unitID);
+
+	targetX, targetY = GetUnitPosition(nearestID);
+
+	localX, localY = GetUnitPosition(unitID);
+
+	directionx = (targetX - localX);
+	directiony = (targetY - localY);	
+	distancex = math.abs((targetX - localX));
+	distancey = math.abs((targetY - localY));
+
+	totdistance = distancex + distancey;
+
+	doCharge = true;
+	chargeDirection = -1;
+	-- Check if it is appropriate to charge, I.E we can hit 2 people
+	-- Not checking if we are charging friendlies...oh well! BERSERKER
+	--AddText("Do Charge?..." .. doCharge, pixelX, pixelY, 40, 0, 5, 0.4, 20, 255, 20);
+	if (directionx == 1) then
+		secondUnitID = FindUnitByTile(targetX + 1, targetY);
+
+		if (secondUnitID ~= -1) then
+			doCharge = true;
+			chargeDirection = 1;
+		end
+	elseif (directionx == -1) then
+		secondUnitID = FindUnitByTile(targetX - 1, targetY);
+		if (secondUnitID ~= -1) then
+			doCharge = true;
+			chargeDirection = 2;
+		end
+	elseif (directiony == 1) then
+		secondUnitID = FindUnitByTile(targetX, targetY + 1);
+		if (secondUnitID ~= -1) then
+			doCharge = true;
+			chargeDirection = 3;
+		end
+	elseif (directiony == -1) then
+		secondUnitID = FindUnitByTile(targetX, targetY - 1);
+		if (secondUnitID ~= -1) then
+			doCharge = true;
+			chargeDirection = 4;
+		end	
+	end
+
+	if (totdistance == 1) then
+		if (doCharge == true) then
+			Charge();
+		else
+			AttackNearest()
+		end
+	end
 end
 
+function Charge()
+	localX, localY = GetUnitPosition(unitID);
+	pixelX, pixelY = TranslateToPixel(localX, localY);
+	AddText("Cavalry CHARGE..." .. chargeDirection, pixelX, pixelY, 0, -40, 5, 0.4, 255, 20, 20);
+
+	IssueOrder("deselectall");
+	IssueOrder("selectunit", unitID);
+	IssueOrder("selectability", 3);
+	
+	deltaX = 0;
+	deltaY = 0;
+	
+	if (chargeDirection == 1) then
+		deltaX = 1;
+		deltaY = 0;
+	elseif (chargeDirection == 2) then
+		deltaX = -1;
+		deltaY = 0;
+	elseif (chargeDirection == 3) then
+		deltaY = 1;
+		deltaX = 0;
+	elseif (chargeDirection == 4) then
+		deltaY = -1;
+		deltaX = 0;
+	end
+	
+	for i = 1, 4 do
+		IssueOrder("selecttile", localX + (i*deltaX), localY + (i*deltaY));
+	end
+	
+end
 function AttackNearest()
 	nearestID = FindNearest(unitID);
 	targetX, targetY = GetUnitPosition(nearestID);
+	
+	localX, localY = GetUnitPosition(unitID);
+	pixelX, pixelY = TranslateToPixel(localX, localY);
+	AddText("Cavalry Attack NEAREST...", pixelX, pixelY, 0, 40, 5, 0.4, 255, 20, 20);	
 	
 	IssueOrder("deselectall");
 	IssueOrder("selectunit", unitID);
