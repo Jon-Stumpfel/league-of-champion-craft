ada9e38bce2b3e2ca576dad32553b5184d95c4d2
rcartier13@gmail.com <rcartier13@gmail.com@c8e355b9-bc22-55f8-5626-cff841d7379f>
2012-11-01 21:12:59 +0000

    Credits are done have scrolling and way to exit

--- trunk/LOCC/CreditsState.cpp
+++ trunk/LOCC/CreditsState.cpp
@@ -17,7 +17,7 @@ CCreditsState::~CCreditsState(void)
 void CCreditsState::Enter(void)
 {
 	CStateStack::GetInstance()->SetRenderTopOnly(false);
-	m_nY = 123;
+	m_nY = 75 - 40 * 7;
 }
 
 void CCreditsState::Exit(void)
@@ -28,7 +28,7 @@ void CCreditsState::Input(INPUT_ENUM input)
 {
 	switch (input)
 	{
-	case INPUT_CANCEL:
+	case INPUT_ACCEPT:
 		{
 			CStateStack::GetInstance()->Pop();
 		}
@@ -37,7 +37,9 @@ void CCreditsState::Input(INPUT_ENUM input)
 
 void CCreditsState::Update(float fElapsedTime)
 {
-
+	if( m_nY > 490 )
+		m_nY = 75 - 40 * 7;
+	m_nY += (int)(100 * fElapsedTime);
 }
 
 void CCreditsState::Render(void)
@@ -47,33 +49,55 @@ void CCreditsState::Render(void)
 	CBitmapFont pBitmapFont;
 	CSGD_TextureManager::GetInstance()->Draw(CGraphicsManager::GetInstance()->GetID(_T("scrollvert")), 80, -10, 1.3f, 1.2f);
 
-	/*pBitmapFont.Print(StringTable::GetInstance()->GetString
-		("Credits:").c_str(), 290, 103, 1.0f, D3DXCOLOR(255,255,255,255));*/
+	pBitmapFont.Print("Back", xPos + 200, 520, .5f, D3DCOLOR_ARGB(255, 204, 153, 51));
 
+	if( m_nY > 75 && m_nY < 490 )
+	{
+		pBitmapFont.Print(StringTable::GetInstance()->GetString
+			("Art: Maher Sagrillo").c_str(), xPos, m_nY, 0.5f, D3DXCOLOR(255,255,255,255));
+	}
+
+	if( m_nY > 75 - offset && m_nY < 490 - offset )
+	{
+		pBitmapFont.Print(StringTable::GetInstance()->GetString
+			("Lead Producer: John O'Leske").c_str(), xPos, m_nY + offset, 0.5f, D3DXCOLOR(255,255,255,255));
+	}
+
+	if( m_nY > 75 - offset * 2 && m_nY < 490 - offset * 2 )
+	{
+		pBitmapFont.Print(StringTable::GetInstance()->GetString
+			("Sounds: Jordan Wells").c_str(), xPos, m_nY + offset * 2, 0.5f, D3DXCOLOR(255,255,255,255));
+	}
+
+	if( m_nY > 75 - offset * 3 && m_nY < 490 - offset * 3 )
+	{
+		pBitmapFont.Print(StringTable::GetInstance()->GetString
+			("Tile Engine: Dalton Gbur").c_str(), xPos, m_nY + offset * 3, 0.5f, D3DXCOLOR(255,255,255,255));
+	}
+
+	if( m_nY > 75 - offset * 4 && m_nY < 490 - offset * 4 )
+	{
+		pBitmapFont.Print(StringTable::GetInstance()->GetString
+			("Core Gameplay: Kyle Veilleux").c_str(), xPos, m_nY + offset * 4, 0.5f, D3DXCOLOR(255,255,255,255));
+	}
+
+	if( m_nY > 75 - offset * 5 && m_nY < 490 - offset * 5 )
+	{
+		pBitmapFont.Print(StringTable::GetInstance()->GetString
+			("Particle Engine: Ryan Cartier").c_str(), xPos, m_nY + offset * 5, 0.5f, D3DXCOLOR(255,255,255,255));
+	}
 	
-	pBitmapFont.Print(StringTable::GetInstance()->GetString
-		("Art: Maher Sagrillo").c_str(), xPos, m_nY, 0.5f, D3DXCOLOR(255,255,255,255));
+	if( m_nY > 75 - offset * 6 && m_nY < 490 - offset * 6 )
+	{
+		pBitmapFont.Print(StringTable::GetInstance()->GetString
+			("Animation Engine: Jon Stumpfel").c_str(), xPos, m_nY + offset * 6, 0.5f, D3DXCOLOR(255,255,255,255));
+	}
 
-	pBitmapFont.Print(StringTable::GetInstance()->GetString
-		("Lead Producer: John O'Leske").c_str(), xPos, m_nY + offset, 0.5f, D3DXCOLOR(255,255,255,255));
-
-	pBitmapFont.Print(StringTable::GetInstance()->GetString
-		("Sounds: Jordan Wells").c_str(), xPos, m_nY + offset * 2, 0.5f, D3DXCOLOR(255,255,255,255));
-
-	pBitmapFont.Print(StringTable::GetInstance()->GetString
-		("Tile Engine: Dalton Gbur").c_str(), xPos, m_nY + offset * 3, 0.5f, D3DXCOLOR(255,255,255,255));
-
-	pBitmapFont.Print(StringTable::GetInstance()->GetString
-		("Core Gameplay: Kyle Veilleux").c_str(), xPos, m_nY + offset * 4, 0.5f, D3DXCOLOR(255,255,255,255));
-	
-	pBitmapFont.Print(StringTable::GetInstance()->GetString
-		("Particle Engine: Ryan Cartier").c_str(), xPos, m_nY + offset * 5, 0.5f, D3DXCOLOR(255,255,255,255));
-
-	pBitmapFont.Print(StringTable::GetInstance()->GetString
-		("Animation Engine: Jon Stumpfel").c_str(), xPos, m_nY + offset * 6, 0.5f, D3DXCOLOR(255,255,255,255));
-
-	pBitmapFont.Print(StringTable::GetInstance()->GetString
-		("Assistant Producer: Robert Martinez").c_str(), xPos, m_nY + offset * 7, 0.4f, D3DXCOLOR(255,255,255,255));
+	if( m_nY > 75 - offset * 7 && m_nY < 490 - offset * 7 )
+	{
+		pBitmapFont.Print(StringTable::GetInstance()->GetString
+			("Assistant Producer: Robert Martinez").c_str(), xPos, m_nY + offset * 7, 0.4f, D3DXCOLOR(255,255,255,255));
+	}
 }
 
 CCreditsState* CCreditsState::GetInstance()
