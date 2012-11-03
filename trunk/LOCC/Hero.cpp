970d1338f2ca2b0f7208278bef7b283043059b01
rcartier13@gmail.com <rcartier13@gmail.com@c8e355b9-bc22-55f8-5626-cff841d7379f>
2012-11-03 03:53:16 +0000

    Extra turn sacrifice ability thing now in

--- trunk/LOCC/Hero.cpp
+++ trunk/LOCC/Hero.cpp
@@ -23,7 +23,7 @@ CHero::CHero(void) : CUnit(UT_HERO)
 	// Finds specified spell for the Hero
 	CAbilityManager * pAM = CAbilityManager::GetInstance();
 
-	GiveSpell(SP_FORT);
+	GiveSpell(SP_SAC);
 	GiveSpell(SP_DEATH);
 	GiveSpell(SP_MIND);
 	GiveSpell(SP_TELE);
