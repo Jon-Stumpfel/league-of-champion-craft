0ebde7020112e1d0e8bd458a7ca6f3e83c6e0988
rcartier13 <rcartier13@gmail.com>
2012-11-11 04:37:06 +0000

    particle fix

--- LOCC/Emitter.cpp
+++ LOCC/Emitter.cpp
@@ -568,6 +568,7 @@ void CEmitter::Update( float fElapsedTime )
 				{
 					m_nNumSpawned = 1;
 					Loop();
+					break;
 				}
 			}
 
@@ -687,7 +688,4 @@ void CEmitter::Loop( void )
 
 	m_vAliveParticles.push_back( tParticle );
 
-	//int i = m_vAliveParticles.size()-1;
-
-	
 }
